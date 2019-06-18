ced38897d11dbb02be3aaf1f44fa3ca9ace95542
Abhinesh Gour <abhinesh.gour@gmail.com>
2019-06-18 23:05:40 +0530

    Resolved error refrence issue

--- new-jwt-app/app/controllers/address.controller.js
+++ new-jwt-app/app/controllers/address.controller.js
@@ -7,65 +7,65 @@ const connUri = process.env.MONGO_LOCAL_CONN_URL || 'mongodb://127.0.0.1:27017/n
 
 module.exports = {
 
-	addAddress: (req, res) => {
+  addAddress: (req, res) => {
 
-		mongoose.connect(connUri, { useNewUrlParser: true }, (err) => {
-			let result = {};
-			let status = 201;
-			const tokenData = req.decoded;
-			console.log('tokenData', tokenData);
+    mongoose.connect(connUri, { useNewUrlParser: true }, (err) => {
+      let result = {};
+      let status = 201;
+      const tokenData = req.decoded;
+      console.log('tokenData', tokenData);
 
-			let address = req.body;
-			address.userUUID = tokenData.uuid;
+      let address = req.body;
+      address.userUUID = tokenData.uuid;
 
-			if (!err && address) {
-				Address.create(address, (err) => {
-					if (!err) {
-						result = HttpData(status, 'Address Created successfully');
-						result.address = address;
-					} else {
-						status = 409;
-						result = HttpData(status, 'Address Already Exists', err);
-					}
-					res.status(status).send(result);
-				});
-			} else {
-				status = 500;
-				result = HttpData(status, null, err);
-				res.status(status).send(result);
-			}
+      if (!err && address) {
+        Address.create(address, (err) => {
+          if (!err) {
+            result = HttpData(status, 'Address Created successfully');
+            result.address = address;
+          } else {
+            status = 409;
+            result = HttpData(status, 'Address Already Exists', err);
+          }
+          res.status(status).send(result);
+        });
+      } else {
+        status = 500;
+        result = HttpData(status, null, err);
+        res.status(status).send(result);
+      }
 
-		});
-	},
+    });
+  },
 
-	getAddress: (req, res) => {
+  getAddress: (req, res) => {
 
-		mongoose.connect(connUri, { useNewUrlParser: true }, (err) => {
+    mongoose.connect(connUri, { useNewUrlParser: true }, (err) => {
 
-			let result = {};
-			let status = 200;
-			const reqUserUUID = req.params.userUUID;
+      let result = {};
+      let status = 200;
+      const reqUserUUID = req.params.userUUID;
 
-			if (!err) {
-				Address.findOne({ userUUID: reqUserUUID }, function (err, address) {
-					if (!err) {
-						console.log('address', address);
-						result = HttpData(status, '');
-						result.address = address;
-						res.status(status).send(result);
-					} else {
-						status = 500;
-						result = HttpData(status, null, err);
-						res.status(status).send(result);
-					}
-				});
-			} else {
-				status = 500;
-				result = HttpData(status, null, err);
-				res.status(status).send(result);
-			}
-		});
-	}
+      if (!err) {
+        Address.findOne({ userUUID: reqUserUUID }, function (err, address) {
+          if (!err) {
+            console.log('address', address);
+            result = HttpData(status, '');
+            result.address = address;
+            res.status(status).send(result);
+          } else {
+            status = 500;
+            result = HttpData(status, null, err);
+            res.status(status).send(result);
+          }
+        });
+      } else {
+        status = 500;
+        result = HttpData(status, null, err);
+        res.status(status).send(result);
+      }
+    });
+  }
 
 
 
