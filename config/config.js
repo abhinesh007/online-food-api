50139f05d17b786a0cd8ba33753584a1363d48f7
Abhinesh Gour <abhinesh.gour@gmail.com>
2019-06-25 15:33:39 +0530

    Removed db URL

--- config/config.js
+++ config/config.js
@@ -6,6 +6,5 @@ module.exports = {
   production: {
     port: process.env.PORT || 5000,
     saltingRounds: 10
-  },
-  dbUrl: 'mongodb://test1:abhinesh77@ds121301.mlab.com:21301/heroku_1ld3k6rl'
+  }
 }
